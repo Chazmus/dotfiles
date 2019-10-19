5c0c5b9f57e0d5f60e3013a03e1d6e18614edf31
Chaz Bailey <charles.s.bailey@gmail.com>
2019-10-19 20:11:17 +0800

    Add a windows provisioning script

---
+++ windows-provisioning.ps1
@@ -0,0 +1,13 @@
+# Install Chocolatey
+# Must run as admin before running this script: 
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+# Install my MFING packages
+choco install git -y
+choco install googlechrome -y
+choco install steam -y
+choco install jetbrainstoolbox -y
+choco install unity-hub -y
+choco install geforce-experience -y
+choco install python -y
+choco install vscode -y
