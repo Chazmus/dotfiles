d6fa6259e20b8391bcd91800c19e0170127d5524
Chaz Bailey <charles.s.bailey@gmail.com>
2021-07-02 15:18:28 +0100

    Update ideavimrc and windows provisioning script

--- windows-provisioning.ps1
+++ windows-provisioning.ps1
@@ -1,30 +1,51 @@
-# Install Chocolatey
-# Must run as admin before running this script: 
-# Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-# Install my MFING packages
+Function Invoke-Main {
+    # Install Chocolatey
+    # Must run as admin before running this script: 
+    if (Test-CommandExists "choco")
+    {
+        Write-Output "Chocolatey is installed... installing packages"
+    }
+    else
+    {
+        Write-Output "Chocolatey not found, installing chocolatey..."
+        Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+    }
 
-# The always list
-choco install git -y
-choco install vim -y
-choco install googlechrome -y
-choco install jetbrainstoolbox -y
-choco install vscode -y
-choco install openoffice -y
-choco install gimp -y
-choco install spotify -y
-choco install steam -y
-choco install twitch -y
-choco install discord -y
-choco install 7zip -y
+    # Install my MFING packages
+    # The always list
+    choco install git -y
+    choco install vim -y
+    choco install googlechrome -y
+    choco install jetbrainstoolbox -y
+    choco install vscode -y
+    choco install openoffice -y
+    choco install gimp -y
+    choco install spotify -y
+    choco install steam -y
+    choco install twitch -y
+    choco install discord -y
+    choco install 7zip -y
 
-# Maybe?
-choco install utorrent -y
-choco install adobereader -y
-choco install geforce-experience -y
-choco install audacity -y
+    # Maybe?
+    # choco install utorrent -y
+    # choco install adobereader -y
+    # choco install geforce-experience -y
+    # choco install audacity -y
 
-# Extra dev stuff 
-choco install python -y
-choco install nodejs -y
-choco install openjdk -y
-choco install cmdermini -y
+    # Extra dev stuff 
+    choco install python -y
+    choco install nodejs -y
+    choco install openjdk -y
+    choco install cmdermini -y
+}
+
+Function Test-CommandExists {
+    Param ($command)
+    $oldPreference = $ErrorActionPreference
+    $ErrorActionPreference = "stop"
+    try { if (Get-Command $command) { RETURN $true } }
+    Catch { Write-Host "$command does not exist"; RETURN $false }
+    Finally { $ErrorActionPreference = $oldPreference }
+}
+
+Invoke-Main
